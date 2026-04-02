ba1a32d2822832c911acd012de95f24ba82f5bb5
Tysha Barksdale <tysha.barksdale@gmail.com>
2026-04-02 17:26:43 +0000

    Can you change logo only in header to be left aligned in the container o

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -46,9 +46,9 @@ export function Navbar() {
       transition={{ duration: 0.8, ease: "easeOut" }}
       className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
     >
-      <div className="container mx-auto flex h-24 lg:h-40 items-center lg:justify-between px-4 lg:pl-0 lg:pr-12 xl:pr-20 relative max-w-[1920px]">
+      <div className="flex h-24 lg:h-40 items-center justify-between w-full px-4 lg:pl-0 lg:pr-12 xl:pr-20 relative">
         {/* Logo - Centered on Mobile/Tablet (Portrait), Far Left on Desktop/Tablet (Landscape) */}
-        <Link href="/" className="flex items-center group py-2 mx-auto lg:mx-0 lg:ml-0">
+        <Link href="/" className="flex items-center group py-2 mx-auto lg:mx-0">
           <div className="relative h-20 w-64 lg:h-36 lg:w-[500px]">
             <Image 
               src={headerLogoUrl}
